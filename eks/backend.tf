649b7de7d12263baeb5bc45098211d99b8bc81db
Ayesha2024 <122157209+Ayesha2024@users.noreply.github.com>
2024-08-15 19:59:46 +0500

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -10,7 +10,7 @@ terraform {
     bucket         = "my-ews-baket6"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    
     encrypt        = true
   }
 }
